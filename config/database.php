ef0beab1af5fe8d5f1aea6e8fd0606ceae66223a
capstone student <capstonestudent@pn.local>
2020-02-19 21:05:07 +0800

    [ADD] database configuration

--- config/database.php
+++ config/database.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Str;
 
+
+$DATABASE_URL = parse_url('postgres://shudphncipqaqb:8651b2869fb8f9e353ca4ff18d6057c739b17db43d0c6487c40c6b439f9fe772@ec2-35-168-54-239.compute-1.amazonaws.com:5432/d6uib83am67f39
+');
 return [
 
     /*
@@ -15,7 +18,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,12 +68,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => $DATABASE_URL['url'],
+            'host' => $DATABASE_URL['host'],
+            'port' => $DATABASE_URL['port'],
+            'database' => ltrim($DATABASE_URL['path'],'/'),
+            'username' => $DATABASE_URL['username'],
+            'password' => $DATABASE_URL['password'],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
