09fd0bee65a031533d564b09074228ef239ca103
capstone student <capstonestudent@pn.local>
2020-03-01 00:14:38 +0800

    [add] grades factory

--- database/factories/GradesFactory.php
+++ database/factories/GradesFactory.php
@@ -6,8 +6,11 @@ use SMS\Models\Grades;
 use Faker\Generator as Faker;
 
 $factory->define(Grades::class, function (Faker $faker) {
+
+    $id = $faker->randomElement([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]);
+
     return [
-        'subject_id' =>$faker->numberBetween(1,11),
+        'subject_id' =>$id,
         'teacher_id' =>$faker->numberBetween(1,50),
         'class_id' => $faker->numberBetween(1,6),
         'user_id' => $faker->numberBetween(1,250),
