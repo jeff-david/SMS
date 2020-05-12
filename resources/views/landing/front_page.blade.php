d068a6fde56fa2e423ea2d781fca4c06b3ab72c0
jeff <jdpotrido@student.passerellesnumeriques.org>
2020-05-12 09:41:49 +0700

    [update] sample

--- resources/views/landing/front_page.blade.php
+++ resources/views/landing/front_page.blade.php
@@ -42,18 +42,18 @@
     <div class="container">
         <div class="row">
             <div class="col-md-6">
-                <h1 class="title">Jessica</h1>
+                <h1 class="title">Mission(PUI Jessica)</h1>
                 <h4>Inspired by her patroness, the Virgin of the Poor,
                     the Sisters of Mary Schools shall direct their energy and resources to the poorest of the poor youth
                     of the country by way of providing them with high quality Secondary Education intensive on
                     Vocational-Technical Curriculum molding them into citizens committed to serve the nation, to love
-                    their fellow being, and spread moral and spiritual values based on the Gospels. Ekang bAYOT
+                    their fellow being, and spread moral and spiritual values based on the Gospels.
                 </h4>
                 <br>
             </div>
 
             <div class="col-md-6">
-                <h1 class="title">Eloisa</h1>
+                <h1 class="title">Vision</h1>
                 <h4>The Sisters of Mary School envisions that the graduates, in their everyday life and in the pursuit
                     of their calling, will become the new disciples in spreading the work of redemption, and at the same
                     time teach and lead by example the Marian virtues of simplicity, charity, gratitude and joy.
