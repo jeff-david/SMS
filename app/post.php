297514a4628acd0dd93953fbbe1ddaf26696b832
romeolenizo <romeo.lenizo@student.passerellesnumeriques.org>
2020-05-21 11:50:06 +0800

    [add]post and comment, no routing

---
+++ app/post.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace SMS;
+
+use Illuminate\Database\Eloquent\Model;
+
+class post extends Model
+{
+    //
+    use SoftDeletes;
+  
+    protected $dates = ['deleted_at'];
+  
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['title', 'body'];
+   
+    /**
+     * The has Many Relationship
+     *
+     * @var array
+     */
+    public function comments()
+    {
+        return $this->hasMany(Comment::class)->whereNull('parent_id');
+    }
+}
---
+++ resources/views/admin/show.blade.php
@@ -0,0 +1,38 @@
+@extends('layout.admin')
+   
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="text-center text-success">Admin Display comments</h3>
+                    <br/>
+                    <h2>{{ $post->title }}</h2>
+                    <p>
+                        {{ $post->body }}
+                    </p>
+                    <hr />
+                    <h4>Display Comments</h4>
+                    <a href="{{ route('posts.create') }}" class="btn btn-success" style="float: right">Create Post</a>
+  
+                    @include('posts.commentsDisplay', ['comments' => $post->comments, 'post_id' => $post->id])
+   
+                    <hr />
+                    <h4>Add comment</h4>
+                    <form method="post" action="{{ route('comments.store'   ) }}">
+                        @csrf
+                        <div class="form-group">
+                            <textarea class="form-control" name="body"></textarea>
+                            <input type="hidden" name="post_id" value="{{ $post->id }}" />
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" class="btn btn-success" value="Add Comment" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
